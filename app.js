2da62f24dde8820caa0c32fd18b698af3b272435
Anthony Nguyen <TonyNguyen@Anthonys-MacBook-Pro.local>
2015-09-10 12:04:45 -0700

    implemented drop limits and add new nodes

--- app.js
+++ app.js
@@ -1,72 +1,87 @@
 var app = angular.module('testApp', ['ui.tree', 'ngRoute']);
 
 app.controller('RecipeApp', ['$scope', function ($scope) {
-  $scope.recipe = {
-		title: "Chewie Chocolate Chip Cookies", 
+  $scope.recipes = [{
+		title: "Chewie Chocolate Chip Cookies",
+		type: 'recipe', 
 		author: "Tony Nguyen", 
 		image: "http://s3.amazonaws.com/gmi-digital-library/2ea0aafc-2942-4134-947f-847c043411ae.jpg", 
 		description: "Crisp edges, chewy middles.", 
 		score: 0, 
 		vessels: [{
 			vesselName: 'small mixing bowl',
+			type: 'vessel',
 			actions: [{
 				actionName: "mix in",
+				type: 'action',
 				ingredients: [{
 					ingredientName: 'chocolate chips',
-					vesselId: "blank",
+					type: 'ingredient',
      			imperialQuantity: 4.5,
 					imperialUnits: "oz",
-					metricQuantity: 3.5,
-					metricUnits: "gram",
-					ingredientId: 1,
-					// Hover to show details
-					details: "some futher details"
 					}, {
 					ingredientName: 'butter',
-					vesselId: "blank",
+					type: 'ingredient',
      			imperialQuantity: 4.5,
 					imperialUnits: "stick",
-					metricQuantity: 3.5,
-					metricUnits: "stick",
-					ingredientId: 2,
-					// Hover to show details
-					details: "some futher details"
 				}]
 			}]
 		},{
 			vesselName: 'large mixing bowl',
+			type: 'vessel',
 			actions: [{
 				actionName: "slowly mix in",
+				type: 'action',
 				ingredients: [{
 					ingredientName: 'sugar',
-					vesselId: "blank",
+					type: 'ingredient',					
      			imperialQuantity: 1.5,
 					imperialUnits: "cup",
-					metricQuantity: 21,
-					metricUnits: "gram",
-					ingredientId: 4,
-					// Hover to show details
-					details: "some futher details"
 				}]
 			},{ 
 				actionName: "blend in",
+				type: 'action',
 				ingredients: [{
 					ingredientName: 'flour',
-					vesselId: "blank",
+					type: 'ingredient',					
      			imperialQuantity: 2,
 					imperialUnits: "cup",
-					metricQuantity: 32,
-					metricUnits: "gram",
-					ingredientId: 3,
-					// Hover to show details
-					details: "some futher details"
 				}]
 			}]
 		}]
-	};
+	}];
   
+	$scope.movementOptions = {
+		accept: function (sourceNodeScope, destNodeScope, destIndex) {
+			var srcType = sourceNodeScope.$element.attr('data-type');
+			var dstType = destNodeScope.$element.attr('data-type');
+			console.log("source type " + srcType);
+			console.log("dest type " + dstType);
+			console.log(destIndex);
+			if ((srcType === "action" && dstType === "vessel") || 
+				(srcType === "ingredient" && dstType === "action") || 
+				(srcType === "vessel" && dstType === "recipe") ){
+				return true;
+			} else {
+				return false;
+			}
+		} 
+	};
+
+	// Steal node ID format
+ 	$scope.newSubItem = function (scope) {
+    console.log(scope);
+    var nodeData = scope.$modelValue;
+    nodeData.nodes.push({
+      id: nodeData.id * 10 + nodeData.nodes.length,
+      title: nodeData.title + '.' + (nodeData.nodes.length + 1),
+      nodes: []
+    });
+  };
+
   $scope.addVessel = function () {
   	if (this.newVessel){
+  		this.newVessel.type = 'vessel';
   		this.newVessel.actions = [];
 			this.recipe.vessels.push(this.newVessel);
 			console.log(this);
@@ -75,6 +90,7 @@ app.controller('RecipeApp', ['$scope', function ($scope) {
   };
   $scope.addAction = function () {
    	if (this.newAction){
+   		this.newAction.type = 'action';
    		this.newAction.ingredients = [];
 			this.vessel.actions.push(this.newAction);
 			console.log(this);
@@ -83,12 +99,14 @@ app.controller('RecipeApp', ['$scope', function ($scope) {
   };
   $scope.addIngredient = function () {
   	if (this.newIngredient){
+  		// TODO add other parts of ingredients 
+  		this.newIngredient.type = 'ingredient';
 			this.action.ingredients.push(this.newIngredient);
 			console.log(this);
   		this.newIngredient = '';  		
   	}
   };
-  
+
   $scope.vesselConfig = {
       group: { name: 'vesselGroup', pull: true, put: true },
       animation: 50,
